e439d896f2bc8532c0b563a2d8a5527a54aede05
Milan992 <mitic.gsm@gmail.com>
2020-07-01 22:58:09 +0200

    Created MainWindowVIewModel class

---
+++ DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEmployeeRecord.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
