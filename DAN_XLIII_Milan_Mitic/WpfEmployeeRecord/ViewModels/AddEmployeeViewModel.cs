0dabb32ec0e0eec95eba7fffce2c63a706964fd1
Milan992 <mitic.gsm@gmail.com>
2020-07-02 18:24:07 +0200

    Add employee function implemented

---
+++ DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/ViewModels/AddEmployeeViewModel.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfEmployeeRecord.Model;
+using WpfEmployeeRecord.Views;
+
+namespace WpfEmployeeRecord.ViewModels
+{
+    class AddEmployeeViewModel : ViewModelBase
+    {
+        AddEmployee addEmployee;
+        Service service = new Service();
+
+        #region Constructors
+
+        public AddEmployeeViewModel(AddEmployee addEmployeeOpen)
+        {
+            employee = new tblEmployee();
+            addEmployee = addEmployeeOpen;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private tblEmployee employee;
+
+        public tblEmployee Employee
+        {
+            get
+            {
+                return employee;
+            }
+            set
+            {
+                employee = value;
+                OnPropertyChanged("Employee");
+            }
+        }
+
+        private tblManager manager;
+
+        public tblManager Manager
+        {
+            get
+            {
+                return manager;
+            }
+            set
+            {
+                manager = value;
+                OnPropertyChanged("Manager");
+            }
+        }
+
+        private List<tblSector> sectorList;
+
+        public List<tblSector> SectorList
+        {
+            get
+            {
+                return sectorList;
+            }
+            set
+            {
+                sectorList = value;
+                OnPropertyChanged("SectorList");
+            }
+        }
+
+        private List<tblAccessLevel> accessLevelList;
+
+        public List<tblAccessLevel> AccessLevelList
+        {
+            get
+            {
+                return accessLevelList;
+            }
+            set
+            {
+                accessLevelList = value;
+                OnPropertyChanged("AccessLevelList");
+            }
+        }
+
+        private tblSector sector;
+
+        public tblSector Sector
+        {
+            get
+            {
+                return sector;
+            }
+            set
+            {
+                sector = value;
+                OnPropertyChanged("Sector");
+            }
+        }
+
+        private tblAccessLevel accessLevel;
+
+        public tblAccessLevel AccessLevel
+        {
+            get
+            {
+                return accessLevel;
+            }
+            set
+            {
+                accessLevel = value;
+                OnPropertyChanged("AccessLevel");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand save;
+
+        public ICommand Save
+        {
+            get
+            {
+                if (save == null)
+                {
+                    save = new RelayCommand(param => SaveExecute(), param => CanSaveExecute());
+                }
+
+                return save;
+            }
+        }
+
+        private void SaveExecute()
+        {
+            try
+            {
+                tblEmployee newEmployee = service.AddEmployee(Employee);
+                MessageBox.Show("Employee saved.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanSaveExecute()
+        {
+            return true;
+        }
+
+        private ICommand close;
+
+        public ICommand Close
+        {
+            get
+            {
+                if (close == null)
+                {
+                    close = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+
+                return close;
+            }
+        }
+
+        private void CloseExecute()
+        {
+            try
+            {
+                addEmployee.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
--- DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/ViewModels/ModifyFinancesViewModel.cs
+++ DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/ViewModels/ModifyFinancesViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using WpfEmployeeRecord.Model;
 using WpfEmployeeRecord.Views;
 
@@ -55,6 +57,43 @@ namespace WpfEmployeeRecord.ViewModels
                 employeeList = value;
                 OnPropertyChanged("EmployeeList");
             }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand addEmployee;
+
+        public ICommand AddEmployee
+        {
+            get
+            {
+                if (addEmployee == null)
+                {
+                    addEmployee = new RelayCommand(param => AddEmployeeExecute(), param => CanAddEmployeeExecute());
+                }
+
+                return addEmployee;
+            }
+        }
+
+        private void AddEmployeeExecute()
+        {
+            try
+            {
+                AddEmployee addEmployee = new AddEmployee();
+                addEmployee.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanAddEmployeeExecute()
+        {
+            return true;
         }
 
         #endregion
