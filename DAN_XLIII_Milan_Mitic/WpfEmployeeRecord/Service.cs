3dbc1ef3765e8074c81276c30c50bd89f5a0f72a
Milan992 <mitic.gsm@gmail.com>
2020-07-02 10:42:54 +0200

    Implemented method for cheching if employee with entered username and password exists

--- DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/Service.cs
+++ DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/Service.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfEmployeeRecord.Model;
 
 namespace WpfEmployeeRecord
 {
     class Service
     {
+        public string Role(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool IsEmployee(string userName, string password)
+        {
+            try
+            {
+                using (EmployeeEntities context = new EmployeeEntities())
+                {
+                    tblEmployee employee = (from x in context.tblEmployees where x.UserName == userName && x.Pass == password select x).First();
+                    return true;
+                }
+            }
+            catch 
+            {
+                return false;
+            }
+        }
     }
 }
--- DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/ViewModels/MainWindowViewModel.cs
+++ DAN_XLIII_Milan_Mitic/WpfEmployeeRecord/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfEmployeeRecord.Views;
 
 namespace WpfEmployeeRecord.ViewModels
 {
@@ -17,6 +20,87 @@ namespace WpfEmployeeRecord.ViewModels
         public MainWindowViewModel(MainWindow mainOpen)
         {
             main = mainOpen;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private string userName;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand logIn;
+
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => LogInExecute(), param => CanLogInExecute());
+                }
+
+                return logIn;
+            }
+        }
+
+        private void LogInExecute()
+        {
+            try
+            {
+                if (service.IsEmployee(UserName, Password))
+                {
+                    if (service.Role(UserName, Password) == "emloyee")
+                    {
+                        Employee employee = new Employee();
+                        employee.ShowDialog();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Username or password incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanLogInExecute()
+        {
+            return true;
         }
 
         #endregion
